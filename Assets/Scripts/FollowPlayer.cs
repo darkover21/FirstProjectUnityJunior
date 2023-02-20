0ad58ec1a2c681999acb2c6f977f2067bbe174e4
darkover21 <darkover21@gmail.com>
2023-02-20 15:36:23 +0100

    Follow Player Script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 lastplayerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastplayerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (player.transform.position-lastplayerPos);
+        lastplayerPos = player.transform.position;
+    }
+}
