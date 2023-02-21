0ec246be16402e6b98dc1c5a5a68504fd61cb8df
darkover21 <darkover21@gmail.com>
2023-02-21 18:47:50 +0100

    Adding offset to camera

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   // Increase Camera position same as Player position
         transform.position += (player.transform.position-lastplayerPos);
         lastplayerPos = player.transform.position;
     }
