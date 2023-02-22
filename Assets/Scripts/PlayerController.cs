b8d27f75768c5597d66700063c3c6eb279d67595
darkover21 <darkover21@gmail.com>
2023-02-22 15:37:52 +0100

    turnSpeed for letting car turn right or left

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     // This is the speed variable 
     // It is public so you can see it and change it from editor
     public float speed = 10.0f;
+
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +20,7 @@ public class PlayerController : MonoBehaviour
     {
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // We'll make the vehicle turn right or left.
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
