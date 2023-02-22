d6714785ffec71fcfbe4c0ed348c90cce5bfa7b4
darkover21 <darkover21@gmail.com>
2023-02-22 15:44:03 +0100

    getting Input for turning right or left

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
 
     public float turnSpeed;
+    public float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,9 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
         // We'll make the vehicle turn right or left.
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
